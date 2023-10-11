ca93cba1e45ed3ead0caba8bd9335eeb42843a0d
HoaTran1002 <vanhoa0908751520@gmail.com>
2023-10-12 01:26:39 +0700

    configure route course

--- src/routes/course.route.ts
+++ src/routes/course.route.ts
@@ -6,6 +6,12 @@ const router = Router()
 
 router.get('/getAll', asyncHandelError(getAll))
 router.post('/create', asyncHandelError(courseCreate))
-router.post('/:courseId/roadmap/create', asyncHandelError(courseRoadmap))
+router.post('/:courseId/update')
+router.post('/:courseId/remove')
+router.post('/:courseId/roadmap/:roadmapId/create', asyncHandelError(courseRoadmap))
+router.post('/:courseId/roadmap/:roadmapId/remove', asyncHandelError(courseRoadmap))
+router.post('/:courseId/roadmap/:roadmapId/update', asyncHandelError(courseRoadmap))
 router.post('/:courseId/uploadFile', upload.array('uploadFile'), asyncHandelError(courseUpFiles))
+router.post('/:courseId/file/:fileId/remove', upload.array('uploadFile'), asyncHandelError(courseUpFiles))
+router.post('/:courseId/files/remove', upload.array('uploadFile'), asyncHandelError(courseUpFiles))
 export default router
