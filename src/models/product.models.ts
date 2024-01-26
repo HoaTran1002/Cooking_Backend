f70332ecf69bd63af47ff2bb567b470ff7d414b4
HoaTran1002 <vanhoa0908751520@gmail.com>
2024-01-27 00:43:14 +0700

    deploy

--- src/models/product.models.ts
+++ src/models/product.models.ts
@@ -1,8 +1,17 @@
 import { string } from 'joi'
 import mongoose, { Schema } from 'mongoose'
+import { IImage, IVideo } from '~/interfaces/course.interface'
 import { IProduct } from '~/interfaces/product.interface'
-import { imageSchema, videoSchema } from './course.models'
 
+export const imageSchema = new Schema<IImage>({
+  url: { type: String },
+  key: { type: String }
+})
+
+export const videoSchema = new Schema<IVideo>({
+  url: { type: String },
+  key: { type: String }
+})
 const product = new Schema<IProduct>({
   name: { type: String },
   note: { type: String },
