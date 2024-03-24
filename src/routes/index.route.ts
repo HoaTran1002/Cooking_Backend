73306921d0cc90f3b57b51b82a6507f31891884f
HoaTran1002 <vanhoa0908751520@gmail.com>
2024-03-24 20:33:09 +0700

    ADD routes

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -10,19 +10,32 @@ import courseRoutes from '~/routes/course.route'
 import chefRoutes from '~/routes/chef.route'
 import categoryRoutes from '~/routes/category.route'
 import authRoutes from '~/routes/auth.route'
-import categoryServicesCustomerRoutes from '~/routes/CategoryservicesCustomer.route'
+import categoryServicesCustomerRoutes from '~/routes/categoryServicesCustomer.route'
 import { errorHandeler, notFound } from '~/middlewares/error.middlewear'
-
+import candicateInforRoutes from '~/routes/candicateInfor.route'
+import customerBlogRoutes from '~/routes/customerBlog.route'
+import informationPositionRecruitmentRoutes from '~/routes/InformationPositionRecruitment.route'
+import policyRoutes from '~/routes/policy.route'
+import termAndConditionRoutes from '~/routes/termAndCondition.route'
+import servicesCustomerRoute from '~/routes/servicesCustomer.route'
+import recruitmentBlogRoutes from '~/routes/recruitmentBlog.route'
 // import accountRoutes from '~/routes/account.route'
 // import upload from '~/routes/uploadToS3.route'
 
 const useRoutes = (app: Express) => {
-  app.use('/api/v1/informationBusiness', informationBusiness)
-  app.use('/api/v1/category', categoryRoutes)
-  app.use('/api/v1/customerBlog', customnerBlogRoutes)
+  app.use('/api/v1/informationPositionRecruitment', informationPositionRecruitmentRoutes)
   app.use('/api/v1/CategoryServicesCustomer', categoryServicesCustomerRoutes)
+  app.use('/api/v1/informationBusiness', informationBusiness)
+  app.use('/api/v1/termAndCondition', termAndConditionRoutes)
+  app.use('/api/v1/servicesCustomer', servicesCustomerRoute)
+  app.use('/api/v1/recruitmentBlog', recruitmentBlogRoutes)
+  app.use('/api/v1/candicateInfor', candicateInforRoutes)
+  app.use('/api/v1/customerBlog', customnerBlogRoutes)
+  app.use('/api/v1/customerBlog', customerBlogRoutes)
+  app.use('/api/v1/category', categoryRoutes)
   app.use('/api/v1/product', productRoutes)
   app.use('/api/v1/partner', partnerRoutes)
+  app.use('/api/v1/policy', policyRoutes)
   app.use('/api/v1/course', courseRoutes)
   app.use('/api/v1/auth', authRoutes)
   app.use('/api/v1/tour', tourRoutes)
