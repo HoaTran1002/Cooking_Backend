ecd52477c892c072bd85c652a8e7e6b1b442f9d0
HoaTran1002 <97177639+HoaTran1002@users.noreply.github.com>
2024-05-06 10:33:43 +0700

    Delete dist directory

--- dist/contract/interfaces/partner.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IPartner = void 0;
-var IPartner = /** @class */ (function () {
-    function IPartner() {
-    }
-    return IPartner;
-}());
-exports.IPartner = IPartner;
--- dist/contract/repositories/base.repositories.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BaseRepositoryInterface = /** @class */ (function () {
-    function BaseRepositoryInterface() {
-    }
-    return BaseRepositoryInterface;
-}());
-exports.default = BaseRepositoryInterface;
