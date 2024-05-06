81a8c67a38f5c785392a566d93c790bff0281f40
HoaTran1002 <vanhoa0908751520@gmail.com>
2024-05-06 10:00:15 +0700

    delete

--- dist/services/news.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.newsServiceManager = void 0;
-var newsServiceManager = /** @class */ (function () {
-    function newsServiceManager() {
-    }
-    return newsServiceManager;
-}());
-exports.newsServiceManager = newsServiceManager;
--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -2,7 +2,7 @@ import { Request } from 'express'
 import multer, { Multer } from 'multer'
 import { env } from './env.config'
 import crypto from 'crypto'
-import fs from 'fs' // Import thư viện fs để làm việc với file system
+import fs from 'fs' 
 
 export const uploadMemory = multer({
   storage: multer.memoryStorage()
@@ -11,10 +11,8 @@ export const uploadMemory = multer({
 const storage = multer.diskStorage({
   destination: (req: Request, file: Express.Multer.File, cb) => {
     const uploadPath = env.PATH_DATA_FILE
-    // Kiểm tra xem thư mục uploadPath có tồn tại không
     if (!fs.existsSync(uploadPath)) {
-      // Nếu không tồn tại, hãy tạo thư mục
-      fs.mkdirSync(uploadPath, { recursive: true }) // Sử dụng recursive để tạo các thư mục cha nếu cần thiết
+      fs.mkdirSync(uploadPath, { recursive: true })
     }
     cb(null, uploadPath)
   },
